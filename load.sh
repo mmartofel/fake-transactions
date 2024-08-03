e4846ddf505c4231aa825b166da85d68bef40832
mmartofel <mmartofe@redhat.com>
2024-08-03 19:35:35 +0200

    add simple load generation script

--- load.sh
+++ load.sh
@@ -5,10 +5,13 @@ URL="http://fake-transactions-instana-postgres.apps.c1t8m3zl.westus.aroapp.io/ap
 
 # Infinite loop to repeatedly send requests
 while true; do
+
+for i in {1..10}; do
   curl "$URL"
   echo
   # Optional: Add a sleep interval to avoid overwhelming the server
   sleep 0.1
+done
 
   curl "$URL/count"
   echo " - records in table"  
